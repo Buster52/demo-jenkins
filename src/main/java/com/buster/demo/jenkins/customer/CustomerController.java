31ef2dbb28750c26f6ae5ebac6b3500e9ec0bce3
Edgardo Gonzalez <gonzalezf.e@outlook.com>
2022-02-11 19:20:17 -0500

    oops, fix response

--- src/main/java/com/buster/demo/jenkins/customer/CustomerController.java
+++ src/main/java/com/buster/demo/jenkins/customer/CustomerController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @GetMapping("/name")
     public String getName(){
-        return "Edgardo is the newest customer";
+        return "Edgardo is the oldest customer";
     }
 }
